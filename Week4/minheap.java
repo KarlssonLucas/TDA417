c2e06188e287cffee51faee6d0bd9b0f69d950ee
kakan <lucaskarlsson@live.se>
2020-11-28 18:33:57 +0100

    week4

---
+++ Week4/minheap.java
@@ -0,0 +1,43 @@
+public class minheap {
+    public static void main(String[] args) {
+        int[] arr = [6,8,4,7,2,3,9,1,5]
+        //size == 9
+    }
+}
+
+public class heap {
+    a[] arr;
+    int size;
+
+    public heap(a[] array) {
+        this.arr = array;
+    }
+
+    public int getMin() {
+        return arr[0];
+    }
+
+    public void insert(int n) {
+        if (size >= arr.length) {
+            return;
+        }
+        arr[size] = n;
+        :
+    }
+
+    public int getParent(int pos) {
+
+    }
+
+    public int getleftChild(int pos) {
+
+    }
+
+    public int getrightChild(int pos) {
+
+    }
+
+    public void heapify() {
+
+    }
+}
